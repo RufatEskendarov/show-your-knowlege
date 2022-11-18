e12d88c392a03cb24280e1a46b6f8063a03416e8
Eskendarov <Eskendarov.Rufat@sunbury.ca>
2022-11-17 22:35:44 -0400

    Updated full package

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# show-your-knowlege
-Quiz web app based on JavaScript questions. 
+# Show Your Knowlege
+
+## Description
+
+The knowledge gained on the course motivated me to start creating this project. This project provides a quick test of your knowledge of the JavaScript language. Testing is complicated by the fact that time is limited. Also, the files with the code of this project are great as a tutorial for beginner developers.
+
+Repository: [repo](https://github.com/RufatEskendarov/show-your-knowlege)
+
+WebApp: [webapp]()
+
+## User Story:
+
+AS A coding boot camp student:
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers.
+
+## Acceptance Criteria:
+
+- GIVEN I am taking a code quiz
+- WHEN I click the start button
+  THEN a timer starts and I am presented with a question;
+- WHEN I answer a question
+  THEN I am presented with another question;
+- WHEN I answer a question incorrectly
+  THEN time is subtracted from the clock;
+- WHEN all questions are answered or the timer reaches 0
+  THEN the game is over;
+- WHEN the game is over
+  THEN I can save my initials and score.
+
+## Installation
+
+This application does not require installation, just follow the link:
+
+## Usage
+
+1. By clicking on the link to the webapp, you will be taken to the home page with a welcome text and the "START" button. To start testing, click on the button (Screen-1);
+2. After pressing the 'START' button, the first question will appear on the screen and at the same time the timer in the upper right corner of the timer is designed for 70 seconds. Be careful, each wrong answer will take 10 seconds from the total time (Screen-2);
+3. After answering all questions or running out of time, you will be transferred to the submission page. Enter your initials. The program is not case sensitive, it will automatically convert all initials to uppercase (Serin-3);
+4. By clicking on the submit button, the list with the highest scores will appear on the screen. By clicking on the "Clear sheet" button, you can clear the entire sheet of records, or you can click on the "Go-back" button and move to the home page and start testing a new one (Screen-4).
+
+![Screen-1](./Screenshots/Screen-1.PNG)
+![Screen-2](./Screenshots/Screen-2.PNG)
+![Screen-3](./Screenshots/Screen-3.PNG)
+![Screen-4](./Screenshots/Screen-4.PNG)
+
+## Credits
+
+In this project, I used the methods and templates acquired in the JavaScript course from Jonas Schmidtmann [JonasSchmidtman](https://www.udemy.com/course/the-complete-javascript-course/).
+
+## License
+
+In accordance with https://choosealicense.com/licenses/gpl-3.0/
+
+## Features
+
+- HTML5
+- CSS3
+- JavaScript
+- DOM manipulations
+
+## How to Contribute
+
+Please review my page if you have ideas or advises please contact me (rufat.eskendarov@gmail.com)
+
+## Tests
+
+Manual testing was carried out on the application, no errors or bugs were found. If you encounter problems while using the app, please contact me (rufat.eskendarov@gmail.com).
---
+++ script.js
@@ -0,0 +1,332 @@
+"use strict";
+
+//Quiz questions
+const questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<script>", correct: true },
+      { text: "<scripting>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<javascript>", correct: false },
+    ],
+  },
+  {
+    question: "Where the <script> tag can be used?",
+    answers: [
+      { text: "<head>", correct: false },
+      { text: "<footer>", correct: false },
+      { text: "<body>", correct: false },
+      { text: "<body> and <head>", correct: true },
+    ],
+  },
+  {
+    question: "How do you call a function named 'myFunc'?",
+    answers: [
+      { text: "myFunc()", correct: true },
+      { text: "call myFunc()", correct: false },
+      { text: "call function", correct: false },
+      { text: "get myFunc()", correct: false },
+    ],
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js' ?",
+    answers: [
+      { text: "<script src=", correct: true },
+      { text: "<script name=", correct: false },
+      { text: "<script href", correct: false },
+      { text: "<script link=", correct: false },
+    ],
+  },
+  {
+    question: "How do you write 'Hello' in an alert box?",
+    answers: [
+      { text: "msg('Hello')", correct: false },
+      { text: "alertBox('Hello')", correct: false },
+      { text: "alert('Hello')", correct: true },
+      { text: "msgBox('Hello')", correct: false },
+    ],
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    answers: [
+      { text: "if(i==5)", correct: true },
+      { text: "if i==5 then", correct: false },
+      { text: "if i=5 then", correct: false },
+      { text: "if i=5", correct: false },
+    ],
+  },
+  {
+    question:
+      "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+    answers: [
+      { text: "if (i!=5)", correct: true },
+      { text: "if i=!5 then", correct: false },
+      { text: "if (i<>5)", correct: false },
+      { text: "if i<>5", correct: false },
+    ],
+  },
+  {
+    question: "How does a WHILE loop start?",
+    answers: [
+      { text: "while (i<1;i)", correct: false },
+      { text: "while i= 1 to 10", correct: false },
+      { text: "while (i<=10)", correct: true },
+      { text: "while i if 1", correct: false },
+    ],
+  },
+  {
+    question: "How does a FOR loop start?",
+    answers: [
+      { text: "for i if", correct: false },
+      { text: "for let i=0;", correct: true },
+      { text: "for i 1 to 5", correct: false },
+      { text: "for i++", correct: false },
+    ],
+  },
+  {
+    question: "How do you round the number 7.25, to the nearest integer?",
+    answers: [
+      { text: "round(7.25)", correct: false },
+      { text: "Math.rnd(7.25)", correct: false },
+      { text: "rnd(7.25)", correct: false },
+      { text: "Math.round(7.25)", correct: true },
+    ],
+  },
+];
+
+//Element manipulation assignments
+let shuffledQuiz, currentQuestionIndex;
+const mainWindow = document.getElementById("section-main");
+let submition = document.querySelector(".all-done");
+const labelTimer = document.querySelector(".timer");
+const bestScore = document.querySelector(".best-score");
+const timer = document.getElementById("timer");
+const resultCorrect = document.getElementById("result-correct");
+const resultWrong = document.getElementById("result-wrong");
+const questionElement = document.getElementById("question");
+const answerBtnsElement = document.getElementById("answer-btns");
+const welcomeMessage = document.getElementById("welcome-txt");
+const sectionBtn = document.getElementById("section-start--btn");
+const sectionQuestion = document.getElementById("section-question");
+const submit = document.querySelector(".input-submit");
+const inputInitials = document.getElementById("initials");
+const scoreList = document.querySelector(".score-list");
+const uList = document.getElementById("list");
+const goBack = document.getElementById("go-back");
+const clear = document.getElementById("clear");
+
+const startBtn = document.getElementById("start-btn");
+
+///Assignments for mutable variables
+let time = 0;
+let totalScore = 0;
+let bestScoreList = [];
+let initials = {};
+
+//Event handler forsubmition button.
+submit.addEventListener("click", submitionForm);
+
+//Event handler for starting Quiz
+startBtn.addEventListener("click", startQuiz);
+
+//Event function for "GO back" button
+goBack.addEventListener("click", function () {
+  submition.classList.add("hide");
+  sectionBtn.classList.remove("hide");
+  welcomeMessage.classList.remove("hide");
+  sectionQuestion.classList.add("hide");
+  scoreList.classList.add("hide");
+  timer.classList.add("hide");
+  totalScore = 0;
+});
+
+//Event handler for "Show best score" button
+bestScore.addEventListener("click", function () {
+  welcomeMessage.classList.add("hide");
+  sectionBtn.classList.add("hide");
+  sectionQuestion.classList.add("hide");
+  submition.classList.add("hide");
+  if (!uList.firstChild) {
+    renderScore();
+  }
+  scoreList.classList.remove("hide");
+});
+
+//Clear all best scores list and local storage
+clear.addEventListener("click", function () {
+  bestScoreList = [];
+  localStorage.clear();
+  uList.innerHTML = "";
+});
+
+//Function for begining quiz
+function startQuiz() {
+  welcomeMessage.classList.add("hide");
+  sectionBtn.classList.add("hide");
+  sectionQuestion.classList.remove("hide");
+  shuffledQuiz = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  time = 70;
+  setNextQuestion();
+  startFinishTimer();
+  timer.classList.remove("hide");
+}
+
+//Function to stop quize
+function quizOver() {
+  submition.classList.remove("hide");
+  sectionBtn.classList.add("hide");
+  welcomeMessage.classList.add("hide");
+  sectionQuestion.classList.add("hide");
+  timer.classList.add("hide");
+}
+
+//Setting the next question after the answer
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuiz[currentQuestionIndex]);
+  currentQuestionIndex++;
+}
+
+//Function to render answers
+function showQuestion(question) {
+  if (currentQuestionIndex === shuffledQuiz.length) {
+    sectionQuestion.classList.add("hide");
+    submition.classList.remove("hide");
+  } else {
+    questionElement.innerText = question.question;
+    question.answers.forEach((answer) => {
+      const btn = document.createElement("button");
+      btn.innerText = answer.text;
+      btn.classList.add("answer-btn");
+      if (answer.correct) {
+        btn.dataset.correct = answer.correct;
+      }
+      btn.addEventListener("click", selectAnswer);
+      answerBtnsElement.appendChild(btn);
+    });
+  }
+}
+
+//Function too reset wrong and correct answers
+function resetState() {
+  while (answerBtnsElement.firstChild) {
+    answerBtnsElement.removeChild(answerBtnsElement.firstChild);
+    resultWrong.classList.add("hide");
+    resultCorrect.classList.add("hide");
+    mainWindow.classList.remove("correct");
+    mainWindow.classList.remove("wrong");
+  }
+}
+
+//Checking answer for corrector wrong features
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const correct = selectedBtn.dataset.correct;
+  setStatusClass(mainWindow, correct);
+  if (correct) {
+    totalScore += 5;
+    resultCorrect.classList.remove("hide");
+  } else {
+    resultWrong.classList.remove("hide");
+    time -= 10;
+  }
+  setStatusClass(selectedBtn, correct);
+  setTimeout(setNextQuestion, 1500);
+}
+
+//Identifies correct and incorrect answers
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+//clears the styles adopted as a result of a correct or incorrect answer
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+//Timer and its configurations
+function startFinishTimer() {
+  //Set time to 1 min 10 sec
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    labelTimer.textContent = `${min}:${sec}`;
+
+    if (
+      time === 0 ||
+      !submition.classList.contains("hide") ||
+      !scoreList.classList.contains("hide")
+    ) {
+      clearInterval(timer);
+      quizOver();
+      time = 0;
+    } else if (
+      time < 0 ||
+      !submition.classList.contains("hide") ||
+      !scoreList.classList.contains("hide")
+    ) {
+      time = 0;
+      clearInterval(timer);
+      quizOver();
+    }
+    time--;
+  };
+
+  // Call the timer every sec
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+
+  // When 0 sec, stop timer
+}
+
+//Submission function, triggered after the end of the course or the end of the time
+function submitionForm() {
+  initials.initials = inputInitials.value.toUpperCase();
+  initials.score = totalScore;
+  bestScoreList.push(initials);
+  timer.classList.add("hide");
+  submition.classList.add("hide");
+  renderScore();
+  scoreList.classList.remove("hide");
+  updateStorage();
+}
+
+//Function that adds scores to local storage
+function updateStorage() {
+  localStorage.setItem("scores", JSON.stringify(bestScoreList));
+  bestScoreList = [];
+
+  bestScoreList = JSON.parse(localStorage.getItem("scores"));
+}
+
+//A function that renders records from local storage into a list with the highest scores
+function renderScore() {
+  if (uList.firstChild) {
+    uList.removeChild(uList.firstChild);
+    for (let i = 0; i < bestScoreList.length; i++) {
+      const li = document.createElement("li");
+      li.innerText = `${i + 1}. ${bestScoreList[i].initials} -${
+        bestScoreList[i].score
+      }`;
+      uList.appendChild(li);
+    }
+  } else {
+    for (let i = 0; i < bestScoreList.length; i++) {
+      const li = document.createElement("li");
+      li.innerText = `${i + 1}. ${bestScoreList[i].initials} -${
+        bestScoreList[i].score
+      }`;
+      uList.appendChild(li);
+    }
+  }
+}
